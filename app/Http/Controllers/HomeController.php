d1ac57fa3aa6ad4d95bfda25175a5232f7ed0fb6
Rafael <rafagomes475@gmail.com>
2023-01-11 11:31:14 -0300

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function pokemon($nome)
+    {
+       return view('pokemon', compact('nome'));
+    }
+}
